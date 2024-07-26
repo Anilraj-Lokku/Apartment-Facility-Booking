f9785e063572e6d77ad441f8fb9dbaf2c3b0946e
kedhareswarbattina <battinakedhareswar@gmail.com>
2024-07-26 13:35:35 +0530

    added Manager Entity - eswar

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/apartment/facilities/entity/Manager.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/apartment/facilities/entity/Manager.java
@@ -1,5 +1,61 @@
 package com.apartment.facilities.entity;
 
-public class Manager {
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+
+public class Manager {
+	 @Id
+	    @GeneratedValue(strategy = GenerationType.IDENTITY)
+	    private Integer id;
+	    private String name;
+	    private String phone;
+	    private String email;
+	    
+		public Manager(Integer id, String name, String phone, String email) {
+			super();
+			this.id = id;
+			this.name = name;
+			this.phone = phone;
+			this.email = email;
+		}
+				
+		public Manager() {
+			super();
+		}
+
+		public Integer getId() {
+			return id;
+		}
+		public void setId(Integer id) {
+			this.id = id;
+		}
+		public String getName() {
+			return name;
+		}
+		public void setName(String name) {
+			this.name = name;
+		}
+		public String getPhone() {
+			return phone;
+		}
+		public void setPhone(String phone) {
+			this.phone = phone;
+		}
+		public String getEmail() {
+			return email;
+		}
+		public void setEmail(String email) {
+			this.email = email;
+		}
+
+		@Override
+		public String toString() {
+			return "Manager [id=" + id + ", name=" + name + ", phone=" + phone + ", email=" + email + "]";
+		}
+				
 }
