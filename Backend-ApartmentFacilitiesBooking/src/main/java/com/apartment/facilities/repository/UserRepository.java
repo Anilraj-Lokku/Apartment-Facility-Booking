fca7a5dbe6e7ff4255b57b7f757c7301b3b8d666
9347275722 <parvathigorle99@gmail.com>
2024-07-26 13:16:55 +0530

    user repository added - parvathi

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/apartment/facilities/repository/UserRepository.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/apartment/facilities/repository/UserRepository.java
@@ -1,5 +1,11 @@
 package com.apartment.facilities.repository;
 
-public class UserRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+import com.appartment.facilities.entity.User;
+@Repository
+public interface UserRepository  extends JpaRepository<User, Integer>{
+
+	User findByUserName(String username);
 }
