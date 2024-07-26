3a064c8d9da0b1a9601e0f24a45952902d76edaa
kedhareswarbattina <battinakedhareswar@gmail.com>
2024-07-27 01:24:07 +0530

    added manager service - eswar

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/ManagerService.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/ManagerService.java
@@ -1,5 +1,14 @@
 package com.appartment.facilities.service;
 
-public class ManagerService {
+import org.springframework.stereotype.Service;
 
+import com.appartment.facilities.dto.CreateManagerResponseDto;
+import com.appartment.facilities.dto.ManagerDto;
+import com.appartment.facilities.exception.ManagerException;
+
+@Service
+public interface ManagerService {
+	CreateManagerResponseDto createManager(ManagerDto managerDto) throws ManagerException;
+	String updateManager(ManagerDto managerDto,int managerId) throws ManagerException;
+	ManagerDto getManager();
 }
