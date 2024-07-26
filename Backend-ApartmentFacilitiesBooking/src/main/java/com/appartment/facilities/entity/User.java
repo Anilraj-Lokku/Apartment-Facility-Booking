b5613e96d8a93b32979fa34641d4a8638c594577
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-07-26 19:03:24 +0530

    Erros Fixed in User

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/entity/User.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/entity/User.java
@@ -4,8 +4,12 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Inheritance;
+import jakarta.persistence.InheritanceType;
 
-@Entity
+
+@Entity(name = "userTable")
+@Inheritance(strategy = InheritanceType.JOINED)
 public class User {
 	
 	@Id
@@ -15,73 +19,46 @@ public class User {
 	private String password;
 	private String role;
 	private String status;
-	
-	
+
 	public User() {
-		super();
+		
 	}
-
-
-	public User(Integer id, String userName, String password, String role, String status) {
-		super();
-		this.id = id;
+	public User( String userName, String password, String role, String status) {
 		this.userName = userName;
 		this.password = password;
 		this.role = role;
 		this.status = status;
 	}
-
-
 	public Integer getId() {
 		return id;
 	}
-
-
 	public void setId(Integer id) {
 		this.id = id;
 	}
-
-
 	public String getUserName() {
 		return userName;
 	}
-
-
 	public void setUserName(String userName) {
 		this.userName = userName;
 	}
-
-
 	public String getPassword() {
 		return password;
 	}
-
-
 	public void setPassword(String password) {
 		this.password = password;
 	}
-
-
 	public String getRole() {
 		return role;
 	}
-
-
 	public void setRole(String role) {
 		this.role = role;
 	}
-
-
 	public String getStatus() {
 		return status;
 	}
-
-
 	public void setStatus(String status) {
 		this.status = status;
 	}
-
-
 	@Override
 	public String toString() {
 		return "User [id=" + id + ", userName=" + userName + ", password=" + password + ", role=" + role + ", status="
@@ -89,5 +66,4 @@ public class User {
 	}
 	
 	
-
 }
