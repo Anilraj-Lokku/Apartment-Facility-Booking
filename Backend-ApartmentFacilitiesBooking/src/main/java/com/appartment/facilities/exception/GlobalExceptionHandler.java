1e42ad9193dd0fd3dc6254fe691959017bd50c85
kedhareswarbattina <battinakedhareswar@gmail.com>
2024-07-28 03:05:44 +0530

    created residential exception and updated ,need to create LoginException

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/GlobalExceptionHandler.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/GlobalExceptionHandler.java
@@ -4,10 +4,11 @@ import javax.security.auth.login.LoginException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@Controller
+@ControllerAdvice
 public class GlobalExceptionHandler {
 	
 	@ExceptionHandler(BookingException.class)
--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/ResidentException.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/ResidentException.java
@@ -1,5 +1,13 @@
 package com.appartment.facilities.exception;
 
-public class ResidentException {
+public class ResidentException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+
+	public ResidentException(String msg) {
+		super(msg);
+	}
 }
