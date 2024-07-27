dea1ad1c9bcf8100f8cac153d01ec8197f1cb236
kedhareswarbattina <battinakedhareswar@gmail.com>
2024-07-28 03:56:06 +0530

    created LoginException Class

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/GlobalExceptionHandler.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/GlobalExceptionHandler.java
@@ -1,6 +1,5 @@
 package com.appartment.facilities.exception;
 
-import javax.security.auth.login.LoginException;
 
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
