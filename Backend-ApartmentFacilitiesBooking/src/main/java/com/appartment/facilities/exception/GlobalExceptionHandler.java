78411b2cb4e7713bda4633ba7589d2b93f86e620
Anilraj Lokku <anilrajlokku1810@gmail.com>
2024-07-28 02:25:25 +0530

    Added Dto Files and Partially implemented Global Exceptions

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/GlobalExceptionHandler.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/exception/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.appartment.facilities.exception;
 
+import javax.security.auth.login.LoginException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
@@ -20,5 +22,19 @@ public class GlobalExceptionHandler {
 		String errorMessage = ex.getMessage();
 		return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
 	}
+
+	@ExceptionHandler(ManagerException.class)
+    public ResponseEntity<Object> handleManagerException(ManagerException ex) {
+        String errorMessage = ex.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+	
+	
+	@ExceptionHandler(LoginException.class)
+    public ResponseEntity<Object> handleLoginException(LoginException ex) {
+        String errorMessage = ex.getMessage();
+        return new ResponseEntity<>(errorMessage, HttpStatus.BAD_REQUEST);
+    }
+
 
 }
