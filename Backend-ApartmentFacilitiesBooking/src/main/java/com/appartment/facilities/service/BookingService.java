1fafc250e544e6f25edf4d8ace1090653731dbb6
saijanu888 <sai.janardhan8@gmail.com>
2024-07-28 12:21:15 +0530

    updated errors

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/BookingService.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/BookingService.java
@@ -1,44 +1,19 @@
 package com.appartment.facilities.service;
 
 import java.util.List;
-<<<<<<< HEAD
 
-=======
-<<<<<<< HEAD
+import org.springframework.stereotype.Service;
 
 import com.appartment.facilities.dto.BookingDto;
 import com.appartment.facilities.dto.CreateBookingResponseDto;
 import com.appartment.facilities.exception.BookingException;
 
-public class BookingService {
-	 CreateBookingResponseDto createBooking(BookingDto bookingDto) throws BookingException;
+@Service
+public interface BookingService {
+	
+	CreateBookingResponseDto createBooking(BookingDto bookingDto) throws BookingException;
 	List<BookingDto> getAllBooking();
 	String approveOrRejectBooking(int bookingId,boolean isApproved) throws BookingException;
 	BookingDto getBookingById(int bookingId) throws BookingException;
 	BookingDto cancelBooking(int bookingId) throws BookingException;
-=======
->>>>>>> d67dde192e347172ebacde65d0dfcc2411d6e89a
-
->>>>>>> babbb6e0daeb4be4959d6887d149f272728bc3bd
-import org.springframework.stereotype.Service;
-
-import com.appartment.facilities.dto.BookingDto;
-import com.appartment.facilities.dto.CreateBookingResponseDto;
-
-@Service
-public interface BookingService {
-	
-<<<<<<< HEAD
-	CreateBookingResponseDto createBooking(BookingDto bookingDto) extend BookingException ;
-	List<BookingDto> getAllBooking();
-	String approveOrRejectBooking(int bookingId,boolean isApproved) ;
-	BookingDto getBookingById(int bookingId) ;
-	BookingDto cancelBooking(int bookingId) ;
-=======
-	CreateBookingResponseDto createBooking(BookingDto bookingDto) 
-	List<BookingDto> getAllBooking();
-	String approveOrRejectBooking(int bookingId,boolean isApproved)
-	BookingDto getBookingById(int bookingId) 
-	BookingDto cancelBooking(int bookingId) 
->>>>>>> babbb6e0daeb4be4959d6887d149f272728bc3bd
 }
