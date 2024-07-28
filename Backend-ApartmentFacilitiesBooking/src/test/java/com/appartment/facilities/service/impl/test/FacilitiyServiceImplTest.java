85cbc0aca6d09211d6a8ee050356e37568409249
Sai Charan Sapineni <115992736+CharanRoy7845@users.noreply.github.com>
2024-07-28 23:56:40 +0530

    added test case 2e

--- Backend-ApartmentFacilitiesBooking/src/test/java/com/appartment/facilities/service/impl/test/FacilitiyServiceImplTest.java
+++ Backend-ApartmentFacilitiesBooking/src/test/java/com/appartment/facilities/service/impl/test/FacilitiyServiceImplTest.java
@@ -1,8 +1,32 @@
 package com.appartment.facilities.service.impl.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.appartment.facilities.entity.Booking;
+import com.appartment.facilities.exception.BookingException;
 
 @SpringBootTest
 public class FacilitiyServiceImplTest {
+    @Test
+    public void testApproveOrRejectBooking() throws BookingException {
+        Booking booking = new Booking();
+        booking.setId(1);
+        booking.setStatus("Pending");
+
+        when(bookingRepository.findById(1)).thenReturn(Optional.of(booking));
+
+        String result = bookingService.approveOrRejectBooking(1, true);
+
+        assertEquals("booking with id:1 is accepted", result);
+        verify(bookingRepository).save(any(Booking.class));
+    }
 
 }
