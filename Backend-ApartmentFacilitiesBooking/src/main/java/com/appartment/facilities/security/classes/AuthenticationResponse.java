ae1b9352d3f07b284c4d17e98cf5c5eeb50d6c17
AnkitSPatils <patilank27@gmail.com>
2024-07-28 14:10:14 +0530

    Authentication Response

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/security/classes/AuthenticationResponse.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/security/classes/AuthenticationResponse.java
@@ -1,5 +1,14 @@
 package com.appartment.facilities.security.classes;
 
 public class AuthenticationResponse {
+    private final String jwt;
+
+	public AuthenticationResponse(String jwt) {
+		this.jwt = jwt;
+	}
+
+	public String getJwt() {
+		return jwt;
+	}
 
 }
