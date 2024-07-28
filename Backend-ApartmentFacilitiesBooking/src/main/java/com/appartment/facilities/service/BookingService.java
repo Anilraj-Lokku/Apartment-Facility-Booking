a389f143f760910e1df8223602f5181b59e7cdf4
Thrivikram-Rakesh <thrivikramrakesh@gmail.com>
2024-07-28 11:46:21 +0530

    resolved error in manager service

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/BookingService.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/BookingService.java
@@ -1,5 +1,16 @@
 package com.appartment.facilities.service;
 
+import java.util.List;
+
+import com.appartment.facilities.dto.BookingDto;
+import com.appartment.facilities.dto.CreateBookingResponseDto;
+import com.appartment.facilities.exception.BookingException;
+
 public class BookingService {
+	 CreateBookingResponseDto createBooking(BookingDto bookingDto) throws BookingException;
+	List<BookingDto> getAllBooking();
+	String approveOrRejectBooking(int bookingId,boolean isApproved) throws BookingException;
+	BookingDto getBookingById(int bookingId) throws BookingException;
+	BookingDto cancelBooking(int bookingId) throws BookingException;
 
 }
--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/ManagerService.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/ManagerService.java
@@ -12,4 +12,5 @@ public interface ManagerService {
 	CreateManagerResponseDto createManager(ManagerDto managerDto) throws ManagerException;
 	String updateManager(ManagerDto managerDto,int managerId) throws ManagerException;
 	ManagerDto getManager();
+	String approveSignUp(String userName) throws ResidentException;
 }
