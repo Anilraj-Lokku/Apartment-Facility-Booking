b4cfa2ea40eef340375627b3aedbf72b39069642
Sai Charan Sapineni <115992736+CharanRoy7845@users.noreply.github.com>
2024-07-28 23:35:23 +0530

    added test case

--- Backend-ApartmentFacilitiesBooking/src/test/java/com/appartment/facilities/service/impl/test/BookingServiceImplTest.java
+++ Backend-ApartmentFacilitiesBooking/src/test/java/com/appartment/facilities/service/impl/test/BookingServiceImplTest.java
@@ -1,8 +1,31 @@
 package com.appartment.facilities.service.impl.test;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+import java.util.List;
+
+import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import com.appartment.facilities.dto.BookingDto;
+import com.appartment.facilities.entity.Booking;
+
+import io.jsonwebtoken.lang.Arrays;
 
 @SpringBootTest
 public class BookingServiceImplTest {
+@Test
+    public void testGetAllBooking() {
+        Booking booking = new Booking();
+        booking.setId(1);
+        booking.setFacilityId(1);
+        booking.setResidentId(1);
 
+        when(bookingRepository.findAll()).thenReturn(Arrays.asList(booking));
+
+        List<BookingDto> bookings = bookingService.getAllBooking();
+
+        assertEquals(1,bookings.size());
+    }
 }
