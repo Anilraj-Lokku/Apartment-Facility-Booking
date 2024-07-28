6d0bd33a57140a6c9220a4ac3a1ee6ce6b580e1b
Thrivikram-Rakesh <thrivikramrakesh@gmail.com>
2024-07-28 17:29:44 +0530

    partially implemented Custom user detials

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/security/classes/CustomUserDetails.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/security/classes/CustomUserDetails.java
@@ -1,5 +1,36 @@
 package com.appartment.facilities.security.classes;
 
-public class CustomUserDetails {
+import java.util.Collection;
+import java.util.Collections;
 
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import com.appartment.facilities.entity.User;
+
+public class CustomUserDetails implements UserDetails{
+	private static final long serialVersionUID = 1L;
+	
+	private final User user;
+	
+	public CustomUserDetails(User user) {
+		this.user = user;
+	}
+
+	@Override
+	public Collection<? extends GrantedAuthority> getAuthorities() {
+		return Collections.singletonList(new SimpleGrantedAuthority(user.getRole()));
+	}
+
+	@Override
+	public String getPassword() {
+		return user.getPassword();
+	}
+
+	@Override
+	public String getUsername() {
+		return user.getUserName();
+	}
+	
 }
--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/BookingService.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/service/BookingService.java
@@ -11,17 +11,10 @@ import com.appartment.facilities.exception.BookingException;
 @Service
 public interface BookingService {
 	
-<<<<<<< HEAD
 	CreateBookingResponseDto createBooking(BookingDto bookingDto);
 	List<BookingDto> getAllBooking();
 	String approveOrRejectBooking(int bookingId,boolean isApproved);
 	BookingDto getBookingById(int bookingId);
 	BookingDto cancelBooking(int bookingId);
-=======
-	CreateBookingResponseDto createBooking(BookingDto bookingDto) throws BookingException;
-	List<BookingDto> getAllBooking();
-	String approveOrRejectBooking(int bookingId,boolean isApproved) throws BookingException;
-	BookingDto getBookingById(int bookingId) throws BookingException;
-	BookingDto cancelBooking(int bookingId) throws BookingException;
->>>>>>> 1fafc250e544e6f25edf4d8ace1090653731dbb6
+
 }
