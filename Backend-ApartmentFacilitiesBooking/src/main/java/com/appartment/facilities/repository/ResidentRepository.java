f98f6dc7617ddbbbcb0a71937f32cfc186c64845
Krishnamoorthy0430 <krishnamoorthy0704@gmail.com>
2024-07-29 11:14:11 +0530

    Cleared an error in Residenet Facility and ServiceImply - Krish

--- Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/repository/ResidentRepository.java
+++ Backend-ApartmentFacilitiesBooking/src/main/java/com/appartment/facilities/repository/ResidentRepository.java
@@ -1,6 +1,7 @@
 package com.appartment.facilities.repository;
 
-import org.apache.el.stream.Optional;
+import java.util.Optional;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
